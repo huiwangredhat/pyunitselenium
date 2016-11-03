d2a1ae1310718161ce4698a7c67505dd6a6e5fee
sophiawang <huiwang@redhat.com>
2016-11-03 14:47:28 +0800

    search.py

--- search.py
+++ search.py
@@ -1,17 +1,24 @@
 from selenium import webdriver
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+class TestSearchMethods(unittest.TestCase):
 
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.get('https://www.python.org/')
 
     def test_search(self):
-    self.browser = webdriver.Firefox()
-        self.browser.get('https://www.python.org/')
         text = self.browser.find_element_by_id("id-search-field")
         text.send_keys("python")
-        click(find_element_by_id("submit"))
-        
+        button_go = self.browser.find_element_by_id("submit")
+        button_go.click()
+        self.browser.find_element_by_xpath("//h3[text()='Results']")
+
+    def tearDown(self):
+        self.browser.close()
 
 if __name__ == '__main__':
-    unittest.main()
+#    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSearchMethods)
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
